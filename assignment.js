85c0d9be16e7954c132ef7e678086d0a679c21a6
mukarrabin <tausifchowdhury96@gmail.com>
2021-01-22 17:20:28 +0600

    hello

---
+++ assignment.js
@@ -0,0 +1,69 @@
+// #1 FIRST
+/* function kilometerToMeter(meter){
+    var kilometer = meter*1000;
+    return kilometer;
+}
+var result = kilometerToMeter(1);
+console.log(result);
+*/
+
+
+
+// #2 SECOND
+/*function budgetCalculator(a,b,c){
+    var watchCost= watch*50;
+    var mobileCost= mobile*100;
+    var laptopCost= laptop*500;
+
+    totalCost=watchCost+mobileCost+laptopCost;
+    return totalCost;
+}
+var watch=2;
+var mobile=2;
+var laptop=1;
+var allGadget = budgetCalculator(watch, mobile, laptop);
+console.log(allGadget);
+*/
+
+
+
+// #3 THIRD
+/* function hotelCost(day){
+    var rent = 0;
+    if (day <= 10){
+        rent = day*100;
+    }
+    else if( day<=20){
+        var firstPart = 10*100;
+        var remaining = day-10;
+        var secondPart = remaining*80;
+        rent = firstPart+secondPart;
+    }
+    else{
+        var firstPart = 10*100;
+        var secondPart = 10*80;
+        var remaining = day-20;
+        var thirdPart = remaining*50;
+        rent = firstPart+secondPart+thirdPart;
+    }
+    return rent;
+}
+var count = hotelCost(27);
+console.log(count);
+*/
+
+// #4 FOURTH (Incomplete)
+/* function getBigNum(names){
+    var larger = names[0];
+    for(var i=0; i<=names.length; i++){
+        var element = names[i];
+        if (element >larger){
+            larger = element;
+        }
+    }
+    return larger;
+}
+
+var output = getBigNum(['as', 'adwdwdsx78', 'stausif', 'sade', 'qwed']);
+console.log(output);
+*/
